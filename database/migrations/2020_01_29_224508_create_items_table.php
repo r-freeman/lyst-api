8c6b8b14d69a17e83d8f13b37efeb902870f936a
Ryan Freeman <info@ryanfreeman.io>
2020-01-29 22:58:34 +0000

    Added Item model/migration

---
+++ database/migrations/2020_01_29_224508_create_items_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('image');
+            $table->string('item_code');
+            $table->decimal('price', 10, 2);
+            $table->string('url');
+            $table->bigInteger('store_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('store_id')
+                ->references('id')
+                ->on('stores')
+                ->onUpdate('cascade')
+                ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
