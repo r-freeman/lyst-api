61f7edf19e97686ce69d185cf27db7c153b139e8
Ryan Freeman <N00173767@student.iadt.ie>
2020-02-23 19:41:56 +0000

    Added store method to ListController

--- app/Http/Controllers/API/v1/ListController.php
+++ app/Http/Controllers/API/v1/ListController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 use App\ListModel;
+use Validator;
 use Illuminate\Http\Request;
 
 class ListController extends Controller
@@ -16,6 +17,33 @@ class ListController extends Controller
             "status" => "OK",
             "data" => $lists
         ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'is_public' => 'required|integer',
+            'user_id' => 'required|integer|exists:users,id'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                "status" => "error",
+                "errors" => $validator->errors()
+            ], 422);
+        }
+
+        $list = new ListModel();
+        $list->name = $request->input('name');
+        $list->is_public = $request->input('is_public');
+        $list->user_id = $request->input('user_id');
+        $list->save();
+
+        return response()->json([
+            "status" => "OK",
+            "data" => $list
+        ]);
     }
 
     public function show($id)
