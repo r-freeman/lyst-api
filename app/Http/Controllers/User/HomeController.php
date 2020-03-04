41a8dffe300e3c7f196b8eb992d7687a39700233
Ryan Freeman <N00173767@student.iadt.ie>
2020-03-04 18:37:00 +0000

    Renabled role middleware in HomeController

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
   public function __construct()
   {
       $this->middleware('auth');
-//      $this->middleware('role:user');
+      $this->middleware('role:user');
   }
 
   public function index()
